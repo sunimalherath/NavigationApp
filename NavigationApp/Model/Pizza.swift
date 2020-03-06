583cea2ac7cc2a5d76806aab6016c155d3a4bd74
cod3breaker <ravisherath@gmail.com>
2020-03-07 10:59:07 +1100

    initial commit with Pizza model

---
+++ NavigationApp/Model/Pizza.swift
@@ -0,0 +1,15 @@
+//
+//  Pizza.swift
+//  NavigationApp
+//
+//  Created by Sunimal Herath on 7/3/20.
+//  Copyright © 2020 Sunimal Herath. All rights reserved.
+//
+
+import Foundation
+
+struct Pizza {
+    var id: String = ""
+    var name: String = ""
+    var price: Double = 0.0
+}
---
+++ NavigationApp/Screens/PizzaList.swift
@@ -0,0 +1,21 @@
+//
+//  PizzaList.swift
+//  NavigationApp
+//
+//  Created by Sunimal Herath on 7/3/20.
+//  Copyright © 2020 Sunimal Herath. All rights reserved.
+//
+
+import SwiftUI
+
+struct PizzaList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PizzaList_Previews: PreviewProvider {
+    static var previews: some View {
+        PizzaList()
+    }
+}
